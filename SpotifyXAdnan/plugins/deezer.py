a443ecc055f3010e6a7662354635e1d7794585d1
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-28 18:19:31 +0600

    Update deezer.py

--- SpotifyXAdnan/plugins/deezer.py
+++ SpotifyXAdnan/plugins/deezer.py
@@ -1,38 +1,15 @@
-"""MIT License
-
-Copyright (c) 2022 Daniel
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 from pyrogram import filters
-from mbot import AUTH_CHATS, LOG_GROUP,Mbot
+from SpotifyXAdnan import AUTH_CHATS, LOG_GROUP,SpotifyXAdnan
 from deezer import Client
 from os import mkdir
 from random import randint
-from mbot.utils.mainhelper import fetch_tracks,download_songs,parse_deezer_url,thumb_down
+from SpotifyXAdnan.utils.mainhelper import fetch_tracks,download_songs,parse_deezer_url,thumb_down
 
 
 client = Client()
 
 
-@Mbot.on_message(filters.regex(r'https?://.*deezer[^\s]+') & filters.private | filters.regex(r'https?://.*deezer[^\s]+') & filters.command("deezer") & filters.chat(AUTH_CHATS))
+@SpotifyXAdnan.on_message(filters.regex(r'https?://.*deezer[^\s]+') & filters.private | filters.regex(r'https?://.*deezer[^\s]+') & filters.command("deezer") & filters.chat(AUTH_CHATS))
 async def link_handler(_, message):
     link = message.matches[0].group(0)
     try:
