387bc3d9f9ee33bdb75d70093a5414f6758280d4
ADNAN iTUNE <161193483+Adnan69x@users.noreply.github.com>
2024-04-28 18:19:58 +0600

    Update greetings.py

--- SpotifyXAdnan/plugins/greetings.py
+++ SpotifyXAdnan/plugins/greetings.py
@@ -1,34 +1,11 @@
-"""MIT License
-
-Copyright (c) 2022 Daniel
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 from pyrogram.raw.functions import Ping
-from mbot import LOG_GROUP, OWNER_ID, SUDO_USERS, Mbot,AUTH_CHATS
+from SpotifyXAdnan import LOG_GROUP, OWNER_ID, SUDO_USERS, SpotifyXAdnan,AUTH_CHATS
 from os import execvp,sys
 
-@Mbot.on_message(filters.command("start"))
+@SpotifyXAdnan.on_message(filters.command("start"))
 async def start(client,message):
     reply_markup = [[
         InlineKeyboardButton(
@@ -52,16 +29,16 @@ async def start(client,message):
     return await message.reply_text(f"Hello {message.from_user.first_name}, I'm a Simple Music Downloader Bot. I Currently Support Download from Youtube.",
                     reply_markup=InlineKeyboardMarkup(reply_markup))
 
-@Mbot.on_message(filters.command("restart") & filters.chat(OWNER_ID) & filters.private)
+@SpotifyXAdnan.on_message(filters.command("restart") & filters.chat(OWNER_ID) & filters.private)
 async def restart(_,message):
     await message.delete()
-    execvp(sys.executable,[sys.executable,"-m","mbot"])
+    execvp(sys.executable,[sys.executable,"-m","SpotifyXAdnan"])
 
-@Mbot.on_message(filters.command("log") & filters.chat(SUDO_USERS))
+@SpotifyXAdnan.on_message(filters.command("log") & filters.chat(SUDO_USERS))
 async def send_log(_,message):
     await message.reply_document("bot.log")
 
-@Mbot.on_message(filters.command("ping"))
+@SpotifyXAdnan.on_message(filters.command("ping"))
 async def ping(client,message):
     start = datetime.now()
     await client.send(Ping(ping_id=0))
@@ -78,7 +55,7 @@ HELP = {
 }
 
 
-@Mbot.on_message(filters.command("help"))
+@SpotifyXAdnan.on_message(filters.command("help"))
 async def help(_,message):
     button = [
         [InlineKeyboardButton(text=i, callback_data=f"help_{i}")] for i in HELP
@@ -87,14 +64,14 @@ async def help(_,message):
     await message.reply_text(f"Hello **{message.from_user.first_name}**, I'm **@spotify_downloa_bot**.\nI'm Here to download your music.",
                         reply_markup=InlineKeyboardMarkup(button))
 
-@Mbot.on_callback_query(filters.regex(r"help_(.*?)"))
+@SpotifyXAdnan.on_callback_query(filters.regex(r"help_(.*?)"))
 async def helpbtn(_,query):
     i = query.data.replace("help_","")
     button = InlineKeyboardMarkup([[InlineKeyboardButton("Back",callback_data="helphome")]])
     text = f"Help for **{i}**\n\n{HELP[i]}"
     await query.message.edit(text = text,reply_markup=button)
 
-@Mbot.on_callback_query(filters.regex(r"helphome"))
+@SpotifyXAdnan.on_callback_query(filters.regex(r"helphome"))
 async def help_home(_,query):
     button = [
         [InlineKeyboardButton(text=i, callback_data=f"help_{i}")] for i in HELP
